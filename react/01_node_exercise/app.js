31213b8f8579b973eb2ba2c9588dee612771d63b
Mark Eberhard <mce777@gmail.com>
2016-04-23 21:14:56 +0000

    react1

---
+++ react/01_node_exercise/app.js
@@ -0,0 +1,12 @@
+var calc = require('./calculator.js');
+var colors = require('colors');
+
+calc.add(2,3);
+
+calc.sub(100, 40);
+
+calc.mul(2, 10);
+
+calc.div(60, 20);
+
+console.log('hello'.green);
---
+++ react/01_node_exercise/calculator.js
@@ -0,0 +1,19 @@
+var calc = {
+  add: function(num1, num2) {
+    console.log(num1 + num2);
+  },
+
+  sub: function(num1, num2) {
+    console.log(num1 - num2);
+  },
+
+  mul: function(num1, num2) {
+    console.log(num1 * num2);
+  },
+
+  div: function(num1, num2) {
+    console.log(num1 / num2)
+  }
+};
+
+module.exports = calc;
