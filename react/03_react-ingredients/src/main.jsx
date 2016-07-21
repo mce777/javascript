68feecbdabe39865237407e6cff89c57673ad81b
Mark Eberhard <mce777@gmail.com>
2016-07-21 21:53:53 +0200

    Update main.jsx

--- react/03_react-ingredients/src/main.jsx
+++ react/03_react-ingredients/src/main.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var List = require('./components/List.jsx');
-//test
+
 ReactDOM.render(<List />, document.getElementById('ingredients'));
