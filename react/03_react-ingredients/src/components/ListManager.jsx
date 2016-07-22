0765781636d81b6acb3b8f4a0580b5efae8b47f7
marebe <marebe@dhcp-marebes-mbp.sinner-schrader.de>
2016-07-22 10:51:24 +0200

    ingredients done

--- react/03_react-ingredients/src/components/ListManager.jsx
+++ react/03_react-ingredients/src/components/ListManager.jsx
@@ -5,9 +5,30 @@ var ListManager = React.createClass({
     getInitialState: function() {
         return {items: [], newItemText:''};
     },
+    onChange: function (e) {
+        this.setState({newItemText: e.target.value});
+    },
     handleSubmit: function(e) {
-        e.preventDefault();
+        e.preventDefault();     //prevents onClick from being triggered
 
         var currentItems = this.state.items;
+
+        currentItems.push(this.state.newItemText);
+
+        this.setState({items: currentItems, newItemText:''});
+    },
+    render: function () {
+        return (
+            <div>
+                <h3>{this.props.title}</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.onChange} value={this.state.newItemText} />
+                    <button>Add</button>
+                </form>
+                <List items={this.state.items} />
+            </div>
+        );
     }
 });
+
+module.exports = ListManager;
