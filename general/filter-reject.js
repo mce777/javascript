e26de150fe3877540dd3f004a78953c18253c39e
marebe <mark.eberhard@sinnerschrader.com>
2016-08-05 10:58:17 +0200

    filter

---
+++ general/filter-reject.js
@@ -0,0 +1,19 @@
+var animals = [
+    { name: 'Fluffykins', species: 'rabbit' },
+    { name: 'Caro',       species: 'dog' },
+    { name: 'Hamilton',   species: 'dog' },
+    { name: 'Harold',     species: 'fish' },
+    { name: 'Ursula',     species: 'cat' },
+    { name: 'Jimmy',      species: 'fish' }
+]
+
+// will only return what you filter
+var isDog = function(theanimal) {
+    return theanimal.species !== 'dog';
+}
+
+var dogs = animals.filter(isDog);
+console.log(dogs);
+
+// var nodogs = animals.reject(isDog);
+// console.log(nodogs);
