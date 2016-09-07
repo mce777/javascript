5c61bb1aef6a2de0d19d7c93f7458c66d73cb4bc
marebe <mark.eberhard@sinnerschrader.com>
2016-09-07 11:34:00 +0200

    devslopes done

---
+++ react/10_fetch/src/reflux/ingredients-store.jsx
@@ -0,0 +1,40 @@
+var HTTP = require('../services/httpservice');
+var Reflux = require('reflux');
+var Actions = require('./actions.jsx');
+
+var IngredientStore = Reflux.createStore({
+    listenables: [Actions],
+    getIngredients: function () {
+        HTTP.get('/ingredients')
+        .then(function (json) {
+            this.ingredients = json;
+            this.fireUpdate();
+
+        }.bind(this));
+    },
+    postIngredient: function (text) {
+        if (!this.ingredients) {
+            this.ingredients = [];
+        }
+
+        var ingredient = {
+            "text": text,
+            "id": Math.floor(Date.now() / 1000) + text
+        };
+
+        this.ingredients.push(ingredient);
+        this.fireUpdate();
+
+        HTTP.post('/ingredients', ingredient)
+            .then(function (response) {
+                this.getIngredients();
+            }.bind(this));
+
+    },
+    fireUpdate: function () {
+        this.trigger('change', this.ingredients);
+
+    }
+});
+
+module.exports = IngredientStore;
